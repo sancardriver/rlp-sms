ea1ca5d5ace87a013e3ff3f65e22c4021802ce92
Jonathan Starck <hello@notfallsanitaeter-jstarck.de>
2022-12-07 23:03:22 +0100

    Funktion hinzugefügt

--- dist/index.php
+++ dist/index.php
@@ -176,10 +176,6 @@
         <?php
         class ApplicationVersion
         {
-            const MAJOR = 1;
-            const MINOR = 2;
-            const PATCH = 3;
-        
             public static function get()
             {
                 $commitHash = trim(exec('git log --pretty="%h" -n1 HEAD'));
@@ -187,7 +183,7 @@
                 $commitDate = new \DateTime(trim(exec('git log -n1 --pretty=%ci HEAD')));
                 $commitDate->setTimezone(new \DateTimeZone('UTC'));
         
-                return sprintf('v%s.%s.%s-dev.%s (%s)', self::MAJOR, self::MINOR, self::PATCH, $commitHash, $commitDate->format('Y-m-d H:i:s'));
+                return sprintf('v%s.%s.%s-dev.%s (%s)', $commitHash, $commitDate->format('Y-m-d H:i:s'));
             }
         }
         ?>
