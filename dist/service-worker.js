4a833008521090d07129d459babce1e0930ec928
Jonathan Starck <hello@notfallsanitaeter-jstarck.de>
2023-01-19 21:43:41 +0100

    s

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,7 +1,26 @@
-const CACHE_VERSION = 50;
+var cacheName = 'v0.0.9test';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll([
+        '/',
+        '/index.html',
+        '/css/style.css',
+        '/css/style-dark.css',
+        '/js/script.js',
+        '/icon.png',
+        '/img/logo/logo_rlp-sms_2560.png',
+        '/img/logo/logo_rlp-sms_2000.png',
+        '/img/logo/logo_rlp-sms_1500.png',
+        '/img/logo/logo_rlp-sms_1000.png',
+        '/img/logo/logo_rlp-sms_500.png'
+      ]))
+  );
+});
 
 self.addEventListener('message', (event) => {
-    if (event.data === 'SKIP_WAITING') {
-        self.skipWaiting();
-    }
+  if (event.data === 'SKIP_WAITING') {
+      self.skipWaiting();
+  }
 });
