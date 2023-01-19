6035711dded8fa407626d099df903342dc859ab0
Jonathan Starck <hello@notfallsanitaeter-jstarck.de>
2023-01-19 10:24:19 +0100

    Testen

--- src/js/service-worker.js
+++ src/js/service-worker.js
@@ -1,10 +1,20 @@
-var cacheName = 'v0.0.1';
+var cacheName = 'v0.0.2';
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(cacheName)
       .then(cache => cache.addAll([
-        './index.html'
+        '/',
+        '/index.html',
+        '/css/style.css',
+        '/css/style-dark.css',
+        '/js/script.js',
+        '/icon.png',
+        '/img/logo/logo_rlp-sms_2560.png',
+        '/img/logo/logo_rlp-sms_2000.png',
+        '/img/logo/logo_rlp-sms_1500.png',
+        '/img/logo/logo_rlp-sms_1000.png',
+        '/img/logo/logo_rlp-sms_500.png'
       ]))
   );
 });
