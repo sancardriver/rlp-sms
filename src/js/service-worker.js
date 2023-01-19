1ce5dee09c52b05043ea80051b2c07ff548b02a2
Jonathan Starck <hello@notfallsanitaeter-jstarck.de>
2023-01-19 10:33:21 +0100

    Update

--- src/js/service-worker.js
+++ src/js/service-worker.js
@@ -4,7 +4,6 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(cacheName)
       .then(cache => cache.addAll([
-        '/',
         '/index.html',
         '/css/style.css',
         '/css/style-dark.css',
