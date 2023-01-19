5f97b6fb7082924caef7b7df05db6320b8c0e4b0
Jonathan Starck <hello@notfallsanitaeter-jstarck.de>
2023-01-19 11:14:58 +0100

    Test

--- src/js/service-worker.js
+++ src/js/service-worker.js
@@ -24,15 +24,3 @@ self.addEventListener('message', (event) => {
       self.skipWaiting();
   }
 });
-
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function (response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
